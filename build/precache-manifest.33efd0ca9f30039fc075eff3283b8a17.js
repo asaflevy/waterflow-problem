953ad1602f0d94c386d695156c19a269dbc7c2f1
asafart <asaf@artlist.io>
2019-04-22 17:13:42 +0300

    adding gitub page deploy

---
+++ build/precache-manifest.33efd0ca9f30039fc075eff3283b8a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60f71ac56d076da3ce35",
+    "url": "/waterflow-problem/static/js/runtime~main.9ab05a37.js"
+  },
+  {
+    "revision": "536541a1b1ed95671798",
+    "url": "/waterflow-problem/static/js/main.23650a8d.chunk.js"
+  },
+  {
+    "revision": "d7afe43401d313c94a4e",
+    "url": "/waterflow-problem/static/js/2.75163b24.chunk.js"
+  },
+  {
+    "revision": "536541a1b1ed95671798",
+    "url": "/waterflow-problem/static/css/main.463e31ff.chunk.css"
+  },
+  {
+    "revision": "0ebc909688b2d8fdac05b79035f47636",
+    "url": "/waterflow-problem/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f470c9ca57dd5c5c30c40939821dcb0e.js"
+  "/waterflow-problem/precache-manifest.33efd0ca9f30039fc075eff3283b8a17.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/waterflow-problem/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
